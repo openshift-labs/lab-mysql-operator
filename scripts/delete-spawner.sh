c72ad62ed6b2a7385e1ad0f0f8bbf227c2bd9631
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-04-04 09:50:23 +1100

    Delete imagestream objects.

--- scripts/delete-spawner.sh
+++ scripts/delete-spawner.sh
@@ -8,7 +8,7 @@ JUPYTERHUB_NAMESPACE=`oc project --short`
 
 APPLICATION_LABELS="app=$JUPYTERHUB_APPLICATION-$JUPYTERHUB_NAMESPACE,spawner=learning-portal"
 
-PROJECT_RESOURCES="services,routes,deploymentconfigs,secrets,configmaps,serviceaccounts,rolebindings,serviceaccounts,rolebindings,persistentvolumeclaims,pods"
+PROJECT_RESOURCES="services,routes,deploymentconfigs,imagestreams,secrets,configmaps,serviceaccounts,rolebindings,serviceaccounts,rolebindings,persistentvolumeclaims,pods"
 
 oc delete "$PROJECT_RESOURCES" --selector "$APPLICATION_LABELS"
 
