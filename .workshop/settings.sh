ded2fc3c40ba9ba4d1da328b1847f23cdf528858
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-11-21 10:24:52 +1100

    Tag version 1.8.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,5 @@
 WORKSHOP_NAME=lab-mysql-operator
-WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-mysql-operator:1.7
+WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-mysql-operator:1.8
 WORKSHOP_TITLE="Using the MySQL Operator"
 WORKSHOP_DESCRIPTION="A workshop on using the MySQL operator"
 CONSOLE_IMAGE=quay.io/openshift/origin-console:4.1
